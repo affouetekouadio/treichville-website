b72a529f6491bac01f673f8ab63df124d43f3a4d
affouetekouadio <affouetekouadio@gmail.com>
2025-12-02 09:55:34 +0000

    Nouveau test de déploiement

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,17 +30,18 @@
             }
         </style>
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Treichville') }}</title>
 
         <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        {{-- <link rel="icon" href="/favicon.svg" type="image/svg+xml"> --}}
+        <link rel="apple-touch-icon" href="/logo.png">
+        {{-- <link rel="apple-touch-icon" href="/logo.png"> --}}
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         @viteReactRefresh
-        @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+        @vite(['resources/js/app.tsx'])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
