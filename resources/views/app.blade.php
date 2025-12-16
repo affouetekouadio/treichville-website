f3b111b688c19dbcaa6d8f5996870aff06cf3b48
affouetekouadio <affouetekouadio@gmail.com>
2025-12-16 18:28:00 +0000

    New commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta property="og:title" content="MAIRIE DE TREICHVILLE">
+        <meta property="og:description" content="Mairie de Treichville">
+        <meta property="og:image" content="{{ asset('logo.png') }}">
+        <meta property="og:url" content="{{ url()->current() }}">
+        <meta property="og:type" content="website">
+        <meta name="twitter:card" content="summary_large_image">
 
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
         <script>
@@ -31,12 +37,12 @@
             }
         </style>
 
-        <title inertia>{{ config('app.name', 'Treichville') }}</title>
+        <title inertia>{{ config('app.name', 'Maire de Treichville') }}</title>
 
         <link rel="icon" href="/favicon.ico" sizes="any">
-        {{-- <link rel="icon" href="/favicon.svg" type="image/svg+xml"> --}}
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/logo.png">
-        {{-- <link rel="apple-touch-icon" href="/logo.png"> --}}
+        <link rel="apple-touch-icon" href="/logo.png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
